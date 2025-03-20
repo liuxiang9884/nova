a2a4cb5fbb71be80c03eaa716027570b683a5e7f
liuxiang <liuxiang9884@gmail.com>
2025-03-20 23:22:12 +0800

    refactor cmake

---
+++ cmake/settings.cmake
@@ -0,0 +1,7 @@
+#vcpkg
+set(VCPKG_ROOT "$ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake")
+set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/arm64-osx/include")
+set(CMAKE_TOOLCHAIN_FILE ${VCPKG_ROOT} CACHE STRING "Vcpkg toolchain file")
+
+#nova
+set(NOVA_INCLUDE "${CMAKE_SOURCE_DIR}/include")
--- examples/tomlplusplus_demo.cpp
+++ examples/tomlplusplus_demo.cpp
@@ -2,18 +2,30 @@
 // Created by liuxiang on 2025/3/19.
 //
 
-#include <string>
 #include <filesystem>
+#include <iostream>
+#include <string>
 
 #include <toml++/toml.h>
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    std::print("Usage: tomlplusplus_demo filename\n");
+    std::println("Usage: tomlplusplus_demo filename");
   }
 
   const auto filename = argv[1];
-  const auto toml = toml::parse(filename);
+  std::println("input_file = {}", filename);
+  const auto toml = toml::parse_file(filename);
+
+  const std::string_view title = toml["title"].value_or("default");
+  const int32_t version = toml["value"].value_or(0);
+  const float pi = toml["pi"].value_or(3.14);
+  const bool debug = toml["debug"].value_or(true);
+  std::println("title={}, version={}, pi={}, debug={}",
+    title, version, pi, debug);
+
+  auto created_at = toml["created_at"].value<toml::date_time>().value();
+  // std::println("created_at={}", created_at.date.year, created_at.date.month,);
 
   return 0;
 }
