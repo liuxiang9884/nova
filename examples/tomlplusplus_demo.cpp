7c6874410dedd32c975681eb1b91344ca73c6963
liuxiang <liuxiang9884@gmail.com>
2025-03-20 15:55:20 +0800

    add tomlplusplus_demo

--- examples/tomlplusplus_demo.cpp
+++ examples/tomlplusplus_demo.cpp
@@ -1,10 +1,19 @@
 //
 // Created by liuxiang on 2025/3/19.
 //
-#include <iostream>
+
+#include <string>
+#include <filesystem>
+
 #include <toml++/toml.h>
 
-int main() {
-  std::cout << "hello world" << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::print("Usage: tomlplusplus_demo filename\n");
+  }
+
+  const auto filename = argv[1];
+  const auto toml = toml::parse(filename);
+
   return 0;
 }
