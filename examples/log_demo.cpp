d781ac820dc333675cab8d86d604bb5fe200488f
liuxiang <liuxiang9884@gmail.com>
2025-03-31 16:57:07 +0800

    add log demo

---
+++ examples/log_demo.cpp
@@ -0,0 +1,21 @@
+//
+// Created by liuxiang on 2025/3/31.
+//
+
+#include <filesystem>
+#include <string>
+#include <vector>
+
+#include <unistd.h>
+
+
+
+#include <fmt/format.h>
+#include <toml++/toml.h>
+
+#include "nova/utils/log.h"
+
+int main(int argc, const char** argv) {
+
+  return 0;
+}
---
+++ include/nova/utils/log.h
@@ -0,0 +1,14 @@
+//
+// Created by liuxiang on 2025/3/31.
+//
+
+#ifndef LOG_H
+#define LOG_H
+
+#include <quill/Backend.h>
+#include <quill/Frontend.h>
+#include <quill/LogMacros.h>
+#include <quill/Logger.h>
+#include <quill/sinks/ConsoleSink.h>
+
+#endif //LOG_H
