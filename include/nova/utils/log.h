6ae89e05431c86c194956671436f8019c179a03a
liuxiang <liuxiang9884@gmail.com>
2025-04-07 23:18:53 +0800

    use libnova.a for examples

--- include/nova/utils/log.h
+++ include/nova/utils/log.h
@@ -100,22 +100,7 @@ class LogConfig {
     timestamp_pattern_ = value;
   }
 
-  void FromToml(const toml::node_view<const toml::node>& log_node) {
-    const auto log_level =
-        log_node["log_level"].value_or(kDefaultLogLevelString);
-    log_level_ = LogLevelMap[log_level];
-    console_sink_name_ =
-        log_node["console_sink_name"].value_or(kDefaultLogConsoleSinkName);
-    log_file_ = log_node["log_file"].value_or(kDefaultLogFile);
-    backend_thread_name_ =
-        log_node["backend_thread_name"].value_or(kDefaultLogBackendThreadName);
-    backend_cpu_affinity_ = log_node["backend_cpu_affinity"].value_or(
-        kDefaultLogBackendCpuAffinity);
-    format_pattern_ =
-        log_node["format_pattern"].value_or(kDefaultLogFormatPattern);
-    timestamp_pattern_ =
-        log_node["timestamp_pattern"].value_or(kDefaultLogTimestampPattern);
-  }
+  void FromToml(const toml::node_view<const toml::node>& log_node);
 
   [[nodiscard]] LogLevel log_level() const noexcept {
     return log_level_;
--- src/nova/utils/log.cpp
+++ src/nova/utils/log.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "nova/utils/log.h"
+
+namespace nova {
+
+void LogConfig::FromToml(const toml::node_view<const toml::node>& log_node) {
+  const auto log_level =
+      log_node["log_level"].value_or(kDefaultLogLevelString);
+  log_level_ = LogLevelMap[log_level];
+  console_sink_name_ =
+      log_node["console_sink_name"].value_or(kDefaultLogConsoleSinkName);
+  log_file_ = log_node["log_file"].value_or(kDefaultLogFile);
+  backend_thread_name_ =
+      log_node["backend_thread_name"].value_or(kDefaultLogBackendThreadName);
+  backend_cpu_affinity_ = log_node["backend_cpu_affinity"].value_or(
+      kDefaultLogBackendCpuAffinity);
+  format_pattern_ =
+      log_node["format_pattern"].value_or(kDefaultLogFormatPattern);
+  timestamp_pattern_ =
+      log_node["timestamp_pattern"].value_or(kDefaultLogTimestampPattern);
+}
+
+} // namespace nova
