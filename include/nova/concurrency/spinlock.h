ff89d17b8d765e0598abb159eac91378fb424e2b
liuxiang <liuxiang9884@gmail.com>
2025-04-09 15:50:42 +0800

    impl spinlock

--- include/nova/concurrency/spinlock.h
+++ include/nova/concurrency/spinlock.h
@@ -3,3 +3,25 @@
 //
 
 #pragma once
+
+#include <atomic>
+
+namespace nova {
+
+class SpinLock {
+ public:
+  SpinLock() : flag_{ATOMIC_FLAG_INIT} {}
+
+  void lock() {
+    while (flag_.test_and_set(std::memory_order_acquire));
+  }
+
+  void unlock() {
+    flag_.clear(std::memory_order_release);
+  }
+
+ private:
+  std::atomic_flag flag_;
+};
+
+}  // namespace nova
