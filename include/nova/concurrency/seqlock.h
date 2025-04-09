a1be13ebe64471219e19ca928aaf244e930c3a5f
liuxiang <liuxiang9884@gmail.com>
2025-04-09 18:12:04 +0800

    impl seqlock

--- include/nova/concurrency/seqlock.h
+++ include/nova/concurrency/seqlock.h
@@ -8,6 +8,7 @@
 #include <type_traits>
 
 #include "nova/common/hardware.h"
+#include "nova/common/macros.h"
 
 namespace nova {
 template <typename T>
@@ -18,8 +19,32 @@ class SeqLock {
   static_assert(std::is_trivially_copy_assignable_v<T>,
                 "T must satisfy is_trivially_copy_assignable");
 
+  SeqLock() = default;
 
+  NOVA_DEBUG_NOINLINE T load() const noexcept {
+    T copy;
+    std::size_t seq0, seq1;
+    do {
+      seq0 = seq_.load(std::memory_order_acquire);
+      std::atomic_signal_fence(std::memory_order_acq_rel);
+      copy = value_;
+      std::atomic_signal_fence(std::memory_order_acq_rel);
+      seq1 = seq_.load(std::memory_order_acquire);
+    } while (seq0 != seq1 || seq0 & 1);
+    return copy;
+  }
+
+  NOVA_DEBUG_NOINLINE void store(const T &desired) noexcept {
+    std::size_t seq0 = seq_.load(std::memory_order_relaxed);
+    seq_.store(seq0 + 1, std::memory_order_release);
+    std::atomic_signal_fence(std::memory_order_acq_rel);
+    value_ = desired;
+    std::atomic_signal_fence(std::memory_order_acq_rel);
+    seq_.store(seq0 + 2, std::memory_order_release);
+  }
 
  private:
+  alignas(kCacheLineSize) T value_;
+  std::atomic<std::size_t> seq_ = 0;
 };
 }  // namespace nova
