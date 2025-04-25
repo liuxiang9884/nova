c1853b2723db15af74146faa64a26c1f6626a4b5
LianYun <64520821+lianyun0502@users.noreply.github.com>
2025-04-25 16:11:09 +0800

    Update .gitignore and CMake settings for vcpkg support across platforms (#2)
    * feat: 更新 .gitignore 和 CMake 設定以支援不同平台的 vcpkg 包含路徑
    
    * feat: 更新 CMake 設定以支援 vcpkg，並新增 vcpkg 安裝與使用說明至 README

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -1,7 +1,16 @@
 #vcpkg
 set(VCPKG_ROOT "$ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake")
-set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/arm64-osx/include")
-set(CMAKE_TOOLCHAIN_FILE ${VCPKG_ROOT} CACHE STRING "Vcpkg toolchain file")
+if (APPLE)
+    set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/arm64-osx/include")
+elseif (WIN32)
+    set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/x64-windows/include")
+elseif (UNIX)
+    set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/x64-linux/include")
+endif()
+set(CMAKE_TOOLCHAIN_FILE "$ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE STRING "VCPKG toolchain file")
 
+message(STATUS "VCPKG_ROOT: ${VCPKG_ROOT}")
+message(STATUS "VCPKG_INCLUDE: ${VCPKG_INCLUDE}")
+message(STATUS "CMAKE_TOOLCHAIN_FILE: ${CMAKE_TOOLCHAIN_FILE}")
 #nova
 set(NOVA_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/include")
