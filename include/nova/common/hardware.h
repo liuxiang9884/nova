a78b4a82f0efe88d59401e71ff1a0335867f1a07
liuxiang <liuxiang9884@gmail.com>
2025-04-26 19:36:06 +0800

    fix

--- include/nova/common/hardware.h
+++ include/nova/common/hardware.h
@@ -7,7 +7,7 @@
 #include <new>
 
 namespace nova {
-#ifdef __cpp_lib_hardware_interference_size
+#if defined(__cpp_lib_hardware_interference_size)
 constexpr size_t kCacheLineSize = std::hardware_destructive_interference_size;
 #else
 constexpr size_t kCacheLineSize = 64;
