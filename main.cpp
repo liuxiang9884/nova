59bf9be1e8bf7a61b780cfe673d5b58993f7313f
liuxiang <liuxiang9884@gmail.com>
2025-04-27 16:43:46 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
+#include "nova/common/hardware.h"
+
 int main() {
   std::cout << "Hello World!\n";
   std::cout << "123456789012345678901234567890123456789012345678901234567890123"
                "45678901234567890\n";
-  std::cout << std::hardware_destructive_interference_size << std::endl;
+  std::cout << nova::kCacheLineSize << std::endl;
   return 0;
 
 
