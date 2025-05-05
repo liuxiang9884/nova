1c2d81ce35bcb88c5ab2e5f6279218d2e9de9e65
liuxiang <liuxiang9884@gmail.com>
2025-05-05 12:13:44 +0800

    add define property macros

--- include/nova/common/macros.h
+++ include/nova/common/macros.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+// OS macros
 // Operating System Detection
 #define NOVA_OS_WINDOWS 1
 #define NOVA_OS_MACOS 2
@@ -28,6 +29,7 @@
 #endif
 #endif
 
+// Inline macros
 #ifdef NDEBUG
 #define NOVA_DEBUG_NOINLINE
 #else
@@ -36,3 +38,21 @@
 
 #define NOVA_FORCE_INLINE __attribute__((always_inline))
 #define NOVA_FORCE_NOINLINE __attribute__((noinline))
+
+// Define property macros
+// Pass by value
+#define DEFINE_BASIC_PROPERTY(type, name, variable) \
+private: \
+type variable##_; \
+public: \
+void set_##name(type value) { variable##_ = value; } \
+[[nodiscard]] type name() const { return variable##_; }
+
+// Pass by reference
+#define DEFINE_PROPERTY(type, name, variable) \
+private: \
+type variable##_; \
+public: \
+void set_##name(const type& value) { variable##_ = value; } \
+void set_##name(type&& value) { variable##_ = std::move(value); } \
+[[nodiscard]] const type& name() const { return variable##_; }
