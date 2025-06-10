579475309fe1945df97d8c54e6baff9ab3a5be0e
liuxiang <liuxiang9884@gmail.com>
2025-06-10 16:04:53 +0800

    fix settings.cmake

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -2,12 +2,17 @@
 set(VCPKG_ROOT "$ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake")
 if (APPLE)
     set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/arm64-osx/include")
+    set(CMAKE_OSX_SYSROOT "/Library/Developer/CommandLineTools/SDKs/MacOSX15.5.sdk"
+            CACHE STRING "SDK_PATH" FORCE)
+    set(CMAKE_OSX_DEPLOYMENT_TARGET "15.5")
+    set(CMAKE_OSX_ARCHITECTURES "arm64")
 elseif (WIN32)
     set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/x64-windows/include")
 elseif (UNIX)
     set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/x64-linux/include")
 endif()
-set(CMAKE_TOOLCHAIN_FILE "$ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE STRING "VCPKG toolchain file")
+set(CMAKE_TOOLCHAIN_FILE "$ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake"
+        CACHE STRING "VCPKG toolchain file")
 
 message(STATUS "VCPKG_ROOT: ${VCPKG_ROOT}")
 message(STATUS "VCPKG_INCLUDE: ${VCPKG_INCLUDE}")
