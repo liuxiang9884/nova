32b01cc755497260a8564cbf29c9b85bb540d84e
liuxiang <liuxiang9884@gmail.com>
2025-08-07 23:30:40 +0800

    update yyjson include

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -8,4 +8,5 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(cpp-yyjson)
 set(CPP_YYJSON_INCLUDE ${cpp-yyjson_SOURCE_DIR}/include)
+set(CPP_YYJSON_INCLUDE ${CPP_YYJSON_INCLUDE} CACHE STRING "cpp-yyjson include directory" FORCE)
 message(STATUS "CPP_YYJSON_INCLUDE: " ${CPP_YYJSON_INCLUDE})
