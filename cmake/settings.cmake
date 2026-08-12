835c4321a76b65a837ba212918f549ee7abf64c7
liuxiang <liuxiang9884@gmail.com>
2026-08-12 06:13:02 +0000

    fix: isolate vcpkg include paths

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -1,21 +1,22 @@
-#vcpkg
-set(VCPKG_ROOT "$ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake")
+# vcpkg
+if (DEFINED ENV{VCPKG_ROOT})
+    set(NOVA_VCPKG_ROOT "$ENV{VCPKG_ROOT}")
+else ()
+    set(NOVA_VCPKG_ROOT "$ENV{HOME}/vcpkg")
+endif ()
+
 if (APPLE)
-    set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/arm64-osx/include")
     set(CMAKE_OSX_SYSROOT "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
             CACHE STRING "SDK_PATH" FORCE)
     set(CMAKE_OSX_DEPLOYMENT_TARGET "15.5")
     set(CMAKE_OSX_ARCHITECTURES "arm64")
-elseif (WIN32)
-    set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/x64-windows/include")
-elseif (UNIX)
-    set(VCPKG_INCLUDE "$ENV{HOME}/vcpkg/installed/x64-linux/include")
-endif()
-set(CMAKE_TOOLCHAIN_FILE "$ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake"
-        CACHE STRING "VCPKG toolchain file")
+endif ()
 
-message(STATUS "VCPKG_ROOT: ${VCPKG_ROOT}")
-message(STATUS "VCPKG_INCLUDE: ${VCPKG_INCLUDE}")
+set(CMAKE_TOOLCHAIN_FILE
+        "${NOVA_VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+        CACHE FILEPATH "Vcpkg toolchain file")
+
 message(STATUS "CMAKE_TOOLCHAIN_FILE: ${CMAKE_TOOLCHAIN_FILE}")
-#nova
+
+# nova
 set(NOVA_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/include")
